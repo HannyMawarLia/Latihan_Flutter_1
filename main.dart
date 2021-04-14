85aed343d4351be3f3bef5d2cd6a255d39516b1e
Ni Kadek Hanny Mawar Lia Edystara <82079087+HannyMawarLia@users.noreply.github.com>
2021-04-14 21:22:28 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class  My App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold()
+      appBar: AppBar
+      (title: Text("Latihan Stateless Widget Hello Word") ,) ,
+    ), //Appbar
+      body: Text("Hello Word"),
+    AppBar : AppBar(title: Text("Latihan Stateless Widget Hello World"),),
+    body: Center(child: Text("190030731_Ni Kadek Hanny Mawar Lia Edystara")),
+    ),
+    );
+  }
+}
+
